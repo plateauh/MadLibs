9e5f123309b9be6b299809fe93ae48e4f960d4c3
plateauh <70834431+plateauh@users.noreply.github.com>
2021-12-27 00:20:38 +0300

    A logical error

--- MadLibs/MainViewController.swift
+++ MadLibs/MainViewController.swift
@@ -9,11 +9,15 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    var sentence: [String]?
+    
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        if let value = sentence {
+            label.text = "We are having a perfectly \(value[0]) time right now. Later we will \(value[1]) and \(value[2]) in the \(value[3])"
+        }
     }
-
-
 }
 
--- MadLibs/TextFieldsViewController.swift
+++ MadLibs/TextFieldsViewController.swift
@@ -9,10 +9,24 @@ import UIKit
 
 class TextFieldsViewController: UIViewController {
 
+    @IBOutlet weak var adjectiveTF: UITextField!
+    @IBOutlet weak var firstVerbTF: UITextField!
+    @IBOutlet weak var secondVerbTF: UITextField!
+    @IBOutlet weak var nounTF: UITextField!
+    
+    @IBAction func submitButtonPressed(_ sender: UIButton) {
+        dismiss(animated: true)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destination = segue.destination as! MainViewController
+        destination.sentence?.append(adjectiveTF.text ?? "")
+        destination.sentence?.append(firstVerbTF.text ?? "")
+        destination.sentence?.append(secondVerbTF.text ?? "")
+        destination.sentence?.append(nounTF.text ?? "")
     }
     
 
