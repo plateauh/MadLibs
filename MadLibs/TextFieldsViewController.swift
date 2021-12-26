47c01c20067a71259197fb308404602442d3c7ee
plateauh <70834431+plateauh@users.noreply.github.com>
2021-12-26 23:41:59 +0300

    UI partially done

---
+++ MadLibs/TextFieldsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  TextFieldsViewController.swift
+//  MadLibs
+//
+//  Created by Najd Alsughaiyer on 26/12/2021.
+//
+
+import UIKit
+
+class TextFieldsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
